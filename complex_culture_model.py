7771ba6253d49de4769e14555eb6d2368b692e9e
karolaannabarta <44461604+karolaannabarta@users.noreply.github.com>
2023-01-16 10:17:02 +0100

    Update complex_culture_model.py

--- complex_culture_model.py
+++ complex_culture_model.py
@@ -2,34 +2,44 @@
 """
 Created on Tue Apr  6 11:04:09 2021
 
-@author: Karola Anna Barta
+@author: Karola
 """
-# setting up the working directory:
 import os
-os.chdir("path to the working directory")
+#os.chdir("C:\\Users\\bartakarola\\OneDrive - elte.hu\\birdsong_model_conf_imm_lm\\immigrants_with_non_random_repertoires\\half_deviation_from_the_pop\\8000cycles_migr0_002_004\\raw_simulation_data\\")
+os.chdir("D:\\Karola\\birdsong_model_conf_imm_lm\\half_deviation_from_the_pop\\8000cycles_migr006_008_lm04_06\\raw_simulation_data\\")
+#os.chdir("C:\\Users\\bartakarola\\OneDrive - elte.hu\\birdsong_model_conf_imm_lm\\immigrants_with_non_random_repertoires\\half_deviation_from_the_pop\\1000cycles_migr0_002_004\\raw_simulation_data\\")
 
-# importing neccessary packages and functions:
+
 from random import random
-from random import sample, randint
+from random import sample, randint, choices
 import pandas as pd
 import multiprocessing
 import itertools
 
+import time
+from datetime import timedelta
+
+start = time.time()
+
 """ADJUSTABLE PARAMETERS"""
 
-#general variables and parameters:
-num_of_syll=2000 # number of different syllable types in the pool of syllables
-age = [0,1,2,3,4,5,6,7] # age categories birds could have
-prob_surviving=[0.5, 1 , 0.5, 0.5, 0.5, 0.5, 0.5, 0] # probabilities of surviving in a given age cathegory
-syllables = [syll for syll in range(num_of_syll)] # pool of syllables (a vector)
-
-
 #Defining the birds class
+prop_dev = 0.5
+num_of_syll=2000
+
+#general variables and parameters
+age = [0,1,2,3,4,5,6,7]
+prob_surviving=[0.5, 1 , 0.5, 0.5, 0.5, 0.5, 0.5, 0]
+syllables = [syll for syll in range(num_of_syll)] #the x syllables used by the birds
+
+#individual-based variables:
+
+#creating a class called birds:
 class Birds:
-    #the following things should be defined on the individual level:
+    #these things should be individualistic:
     def __init__(self, ID):
         self.id=ID
-        self.age=0 # their default age is 0
+        self.age=0
         if self.age<1: #so if they are juveniles
             self.syll_rep=() #they won't have a repertoire
         self.song=[]
@@ -40,8 +50,7 @@ class Birds:
         self.prob_surviving=prob_surviving[self.age]
 
 
-# Function for social learning:
-    # During learning young individuals get a randomly chosen tutor. The repertoire of this tutor gets copied into a vector in which each element will be found according to the strength of the conformist bias (intensity). The young individual will acquire a randomly chosen syllable from this vector (tutorsrep2). With a given probability, a learning mistake will be made. After this, the final syllable gets implemented in the young individual's production vector by substituting another, randomly chosen syllable. The function takes four parameters: the learning indiidual's identity (individual), a list containing all the birds in the population (l), the strength of conformism (intensity) and the allowed rate of learning mistakes (learning_mistake).
+
 def learning(individual, l, intensity, learning_mistake):
     indi = num_of_syll
     tutorsrep = [] # the production vector of tutors
@@ -55,12 +64,12 @@ def learning(individual, l, intensity, learning_mistake):
     l[individual].tutors_song = pd.DataFrame(data=(tutorsrep))
     
     #learning: randomly from tutors
+    # so everyone has a dataframe of ten of his neighbour's syll_rep 
     for i in set(tutorsrep):
         tutorsrep2 += round(tutorsrep.count(i)**intensity) * [i]
     
     indi=sample(tutorsrep2, 1)
     indi=indi[0]
-    # Does a mistake in learning happen? 'pm' will decide this.
     pm = random()
     if pm <= learning_mistake: # a mistake in learning happens
         #print("A mistake is being made, turning", indi, "into:")
@@ -74,21 +83,21 @@ def learning(individual, l, intensity, learning_mistake):
         #print(indi)
     # changing a RANDOM syllable in the ind's rep to the new, learned syllable
     l[individual].song.loc[randint(0, len(l[individual].song)-1)] = indi
-    # recalculate the relative frequency of syllable types:
+    # normalize back to rel_freq:
     l[individual].syll_rep = l[individual].song[0].tolist()
 
-# The function containing the main simulation cycle:the definition of additional parameters and that of the initial population, the learning of young individuals and the life-cycle main cycle of the simulations
+
 def song_learning(a, migr, lm, ism):
-    num_cycle=8000 #  For how many cycles (years) the simulation should run for?
-    countyears=1 # this variable is here in case we would want to record the changes only in several years
-    num_birds=100 # number of birds in the population
-    learningoccasions = 20 # number of learning occasions/individual/year
-    
-    #individual-based variables:
+    #print(a, migr, lm, ism)
+    num_cycle = 8000
+    countyears=1 # this variable is here in case we would want to record the changes
+                 # only in several years
+    num_birds=100
     indprodveclen = 150 #individual production vector length
     youngreplen = 30 #young individuals' functional repertoire length
+    
+    learningoccasions = 20
 
-    # Defining the initial population:
     birds0=list()
     for bi in range(num_birds):
         birds0.append(Birds(bi))
@@ -100,14 +109,12 @@ def song_learning(a, migr, lm, ism):
     
     
     birds=birds0
-    
-    # Defining vectors and data frames needed for data collection:
     pop10=[]
     pop10=sample(birds,10)
 
     pop10_rep= pd.DataFrame()
     pop10_indrep = pd.DataFrame()
-    younginds_repchange = pd.DataFrame()
+    #younginds_repchange = pd.DataFrame()
 
     new_pop10_rep=pd.DataFrame()
     new_pop10_syllrep = []
@@ -125,23 +132,19 @@ def song_learning(a, migr, lm, ism):
 
     pop10_rep[str(0)] = sum_use/sum(sum_use) 
 
+
+    learnmode='conf'+str(a)[0]+str(a)[2:]
     
-    learnmode='conf'+str(a)[0]+str(a)[2:] # a variable used when writing out files
     
-    # The main cycle designating the years:
     for c in range(num_cycle):
-        # Within a year, individuals learn for a given number of times:
         for lo in range(learningoccasions):
             for i in range(len(birds)):
-                #Only young individuals learn:
                 if birds[i].age==0:
                     birds[i].tutors = sample(birds, 10)
                     birds[i].tutors = sample(birds[i].tutors, 1)
                     learning(i, birds, a, learning_mistake = lm)
         
-        # For the purpose of this study, we collected data on the repertoire of only 10 individuals from the population:
         pop10=sample(birds,10)
-        # Then in each year we collected data on them:
         if (c+1)% countyears == 0:
             new_pop10_rep=pd.DataFrame()
             new_pop10_syllrep = []
@@ -156,13 +159,23 @@ def song_learning(a, migr, lm, ism):
             for i in range(len(syllables)):
                 sum_use.append(new_pop10_rep.loc[new_pop10_rep[0] == i, 0].count())
         
-            pop10_rep[str(c+1)] = sum_use/sum(sum_use) # here I put c+1 because otherwise there would be two columns with the name "0"
+            pop10_rep[str(c+1)] = sum_use/sum(sum_use) # here I put c+1 because otherwise there would be 
+            # two columns with the name "0"
             new_pop_syllrep = []
             for i in range(len(birds)): # searching through the population
                 for ii in range(len(birds[i].syll_rep)): # searching through their syll_rep
                     new_pop_syllrep.append(birds[i].syll_rep[ii])
-        
-        # Each year a proportion of the population passed away according to their individual surviving probability:
+                    
+            # creating a dataframe that will be built based on the current repertoire of the population but it will contain different syllable types; We will use this later, to fill up the immigrants' repertoire.
+        new_pop_syllrep_freq = pd.DataFrame(pd.Series(new_pop_syllrep).value_counts()).rename_axis("syll_types").reset_index()
+        indices_to_replace = sample(range(new_pop_syllrep_freq.shape[0]), round(new_pop_syllrep_freq.shape[0]*prop_dev))
+        sylls_to_replace_with = sample(syllables, round(new_pop_syllrep_freq.shape[0]*prop_dev)) # this is needed as otherwise there would be a chance that one syllable would be selected more than once which would influence the relative frequncies of syllables of immigrants
+        for ind in indices_to_replace:
+            new_pop_syllrep_freq["syll_types"][ind] = sylls_to_replace_with.pop()
+            
+            
+        #next step: some of them gotta die
+        #survivors=list(np.random.choice(birds, int(len(birds)*(1-lm)), replace=False))
         survivors = []
         for i in range(len(birds)):
             birds[i].prob_surviving=prob_surviving[birds[i].age]
@@ -172,30 +185,29 @@ def song_learning(a, migr, lm, ism):
                 # that bird is  a survivor, hurray!
                 survivors.append(birds[i])
     
-        # Collecting the survivors into a separate vector:
         for i in range(len(survivors)):
             survivors[i].age=survivors[i].age+1
-        
-        # collecting data from the last 10 years of the simulations:
+    
         if c >= (num_cycle-10):
             for i in range(len(pop10)):
                 pop10_indrep[str(c)+'_'+str(i)]=pop10[i].syll_rep
-            for i in range(len(new)):
-                if new[i] in survivors and i < 10:
-                    colname = str(c)+'_'+str(i)
-                    younginds_repchange[colname+'_0'] = new[i].syll_rep0
-                    younginds_repchange[colname+'_1'] = new[i].syll_rep
+           # for i in range(len(new)):
+            #    if new[i] in survivors and i < 10:
+             #       colname = str(c)+'_'+str(i)
+              #      younginds_repchange[colname+'_0'] = new[i].syll_rep0
+               #     younginds_repchange[colname+'_1'] = new[i].syll_rep
     
     
-        # Filling up the population with new birds:
+        #creating new birds:
         new=list()
         num_birds = len(birds)
         for uuui in range(num_birds-len(survivors)):
             new.append(Birds(uuui))
         for ni in range(len(new)):
             pm2 = random()
-            if pm2<=migr: #this individual among the new ones is a migrant
-                new[ni].syll_rep=sample(syllables, youngreplen)*round(indprodveclen/youngreplen)
+            if pm2<=migr: #this individual among the new ones is an immigrant. As so, its repertoire will be filled up from a different set of syllables but those will have the same frequency distribution as in the population repertoire.
+                new[ni].syll_rep=choices(new_pop_syllrep_freq["syll_types"], weights = new_pop_syllrep_freq[0], k=indprodveclen)
+                print(new[ni].syll_rep)
                 new[ni].song = pd.DataFrame(data=(new[ni].syll_rep))
             else: #this individual among the new ones is a local
                 new[ni].syll_rep=sample(new_pop_syllrep, youngreplen*round(indprodveclen/youngreplen))
@@ -205,39 +217,11 @@ def song_learning(a, migr, lm, ism):
         
         birds=survivors+new
     
-    # Writing out the necessary files: 
+    #pop_rep.to_csv('poprep_'+learnmode+'_'+str(num_cycle)+'x'+str(learningoccasions)+'sys'+str(num_of_syll)+str(lm)[2:]+str(ism)+'.csv')
     pop10_rep.to_csv('pop10rep_'+learnmode+'_'+str(num_cycle)+'x'+str(learningoccasions)+'sys'+str(num_of_syll)+str(migr)[2:]+str(lm)[2:]+str(ism)[0]+'.csv')
     pop10_indrep.to_csv('pop10_indrep_'+learnmode+'_'+str(num_cycle)+'x'+str(learningoccasions)+'sys'+str(num_of_syll)+str(migr)[2:]+str(lm)[2:]+str(ism)[0]+'.csv')
-    younginds_repchange.to_csv('younginds_repchange'+learnmode+'_'+str(num_cycle)+'x'+str(learningoccasions)+'sys'+str(num_of_syll)+str(migr)[2:]+str(lm)[2:]+str(ism)[0]+'.csv')
-    
-    dmp10 = pd.DataFrame()
-    column_nr = len(pop10_rep.loc[0,])
-    
-    for i in range(column_nr): # getting through columns/years/num_cycle
-        dij10=[]
-        for j in range(column_nr): # getting through columns/years/num_cycle again
-            sumdiff = sum(abs(pop10_rep.iloc[:,i]-pop10_rep.iloc[:,j]))
-            dij10.append(0.5*sumdiff)
-        dmp10[str(i)]=dij10
-    
-    dmp10.to_csv('distmatpop10_'+learnmode+'_'+str(num_cycle)+'x'+str(learningoccasions)+'sys'+str(num_of_syll)+str(migr)[2:]+str(lm)[2:]+str(ism)[0]+'.csv')
+    #younginds_repchange.to_csv('younginds_repchange'+learnmode+'_'+str(num_cycle)+'x'+str(learningoccasions)+'sys'+str(num_of_syll)+str(migr)[2:]+str(lm)[2:]+str(ism)[0]+'.csv')
 
-    lydmp10 = pd.DataFrame()
-    diff=[]
-    values=[]
-    for c in range(num_cycle-10,num_cycle):
-        for r in range(num_cycle-10,num_cycle):
-            if c>r:
-                diff.append(c-r)
-                values.append(dmp10.iloc[r,c])
-    lydmp10[1]=diff
-    lydmp10[2]=values
-    lydmp10 = lydmp10.sort_values([1,2])
-    lydmp10.to_csv('lydmp10_'+learnmode+'_'+str(num_cycle)+'x'+str(learningoccasions)+'sys'+str(num_of_syll)+str(migr)[2:]+str(lm)[2:]+str(ism)[0]+'.csv')
-    
-
-# As the simulations take a long time to run, we used multiprocessing to run those with different parameter-combinations simultaneously.
-# Description of the multiprocessing method can be found at https://docs.python.org/3/library/multiprocessing.html
 
 """ADJUSTABLE PARAMETERS"""
 alpha1 = 1
@@ -245,8 +229,8 @@ alpha2 = 1.1
 alpha3 = 1.2
 
 migr1 = 0.0
-migr2 = 0.01
-migr3 = 0.02
+migr2 = 0.02
+migr3 = 0.04
 
 lm1 = 0.0
 lm2 = 0.1
@@ -262,10 +246,11 @@ params = {
         'ism' : [x for x in range(num_simrep)]
         }
 
-    
+num_comb = len(params['alpha']) * len(params['migr']) * len(params['lm']) * num_simrep
+
 def main():
     keys = list(params)
-    param_combs = pd.DataFrame(columns = ['alpha', 'migr', 'lm', 'ism'], index = [x for x in range(270)])
+    param_combs = pd.DataFrame(columns = ['alpha', 'migr', 'lm', 'ism'], index = [x for x in range(num_comb)])
     rownum = 0
     
     
@@ -283,4 +268,11 @@ def main():
 if __name__ == '__main__':
     __spec__ = "ModuleSpec(name='builtins', loader=<class '_frozen_importlib.BuiltinImporter'>)"
     main()
+
+
+end = time.time()
+
+elapsed_time = end-start
+
+print("elapsed time: " + str(timedelta(seconds=elapsed_time)))
 
